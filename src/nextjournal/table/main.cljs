aa4d96128c2b083e43f6d7066dd4b1d49ad31e14
Martin Kavalar <martin@nextjournal.com>
2025-12-01 16:20:26 +1100

    Move render to views.cljc

--- src/nextjournal/table/main.cljs
+++ src/nextjournal/table/main.cljs
@@ -1,21 +1,10 @@
 (ns nextjournal.table.main
   (:require [nexus.core :as nexus]
+            [nextjournal.table.views :as views]
             [replicant.dom :as r]))
 
 
-(defn render [state]
-  [:div
-   [:p "Number: " (:number state)]
-   [:div
-    [:label "Step size: "]
-    [:input
-     {:value (:step state)
-      :on
-      {:input
-       [[:effects/save [:step] [:fmt/number [:event.target/value]]]]}}]]
-   [:button.btn
-    {:on {:click [[:actions/inc [:number]]]}}
-    "Count!"]])
+
 
 (def !state
   (atom {}))
@@ -27,10 +16,8 @@
    :nexus/actions {:actions/inc (fn inc [state path]
                                   [[:effects/save path (+ (:step state) (get-in state path))]])}
    :nexus/placeholders {:event.target/value (fn event-target-value [{:replicant/keys [dom-event]}]
-                                              (prn :event.target/value dom-event)
                                               (some-> dom-event .-target .-value))
                         :fmt/number (fn fmt-number [_ value]
-                                      (prn :fmt/number value)
                                       (or (some-> value parse-long) 0))}})
 
 (r/set-dispatch! #(nexus/dispatch nexus !state %1 %2))
@@ -39,7 +26,7 @@
   (js/document.getElementById "app"))
 
 (add-watch !state ::render (fn [_ _ _ new-state]
-                             (r/render root-el (render new-state))))
+                             (r/render root-el (views/render new-state))))
 
 ;; Trigger the initial render
 (reset! !state {:number 0, :step 3})
---
+++ src/nextjournal/table/views.cljc
@@ -0,0 +1,16 @@
+(ns nextjournal.table.views)
+
+(defn render [state]
+  [:div
+   [:p "Number: " (:number state)]
+   [:div
+    [:label "Step size: "]
+    [:input
+     {:value (:step state)
+      :on
+      {:input
+       [[:effects/save [:step] [:fmt/number [:event.target/value]]]]}}]]
+   [:button.btn
+    {:on {:click [[:actions/inc [:number]]]}}
+    "Count!"]])
+
