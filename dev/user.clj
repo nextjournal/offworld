4724a02d07d0caf527085c4f38e8bfbd3580bf65
Kimo Knowles <kimo@tuta.io>
2025-12-30 10:56:24 +0800

    [broken] Try a local dependency on clerk

--- dev/user.clj
+++ dev/user.clj
@@ -1,9 +1,13 @@
 (ns user
   (:require [shadow.cljs.devtools.api :as shadow]
-            [shadow.cljs.devtools.server :as shadow-server]))
+            [shadow.cljs.devtools.server :as shadow-server]
+            nextjournal.clerk))
 
 (defn start-shadow! [opts]
   (shadow-server/start!)
   (shadow/watch :app)
   (shadow/watch :portfolio))
 
+(start-shadow! {})
+
+(nextjournal.clerk/serve! {:browse? true})
